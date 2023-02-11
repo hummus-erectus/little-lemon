66d386f9dedbcb40ba789b654d0aafb81ac2700f
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-02-11 20:52:43 +0900

    Added Metatags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ function Footer() {
             <div>
                 <h3>Doormat Navigation</h3>
                 <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Reservations</a></li>
-                    <li><a href="">Order Online</a></li>
-                    <li><a href="">Login</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div>
             <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ function Navbar() {
         <nav>
             <img src={logo} alt="Little Lemon logo"/>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Menu</a></li>
-                <li><a href="">Reservations</a></li>
-                <li><a href="">Order Online</a></li>
-                <li><a href="">Login</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
             </ul>
         </nav>
     )
