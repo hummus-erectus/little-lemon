a1df2b0df8865e067220ed6d76c1e19f7c5feafb
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-02-12 22:31:15 +0900

    Started styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import './App.css'
 
 function App() {
   return (
