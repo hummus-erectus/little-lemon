437452726eca8c0e9e6f259883c27d3a0abd69c9
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-02-13 22:52:09 +0900

    Styled navbar and footer for desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,33 +3,37 @@ import logoVertical from "../assets/logoVertical.png"
 function Footer() {
     return (
         <footer>
-            <img src={logoVertical} alt="Little Lemon logo full size"/>
-            <div>
-                <h3>Doormat Navigation</h3>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">Reservations</a></li>
-                    <li><a href="#">Order Online</a></li>
-                    <li><a href="#">Login</a></li>
-                </ul>
-            </div>
-            <div>
-                <h3>Contact</h3>
-                <ul>
-                    <li>Address</li>
-                    <li>Phone number</li>
-                    <li>email</li>
-                </ul>
-            </div>
-            <div>
-                <h3>Social Media Links</h3>
-                <ul>
-                    <li><a href="">Instagram</a></li>
-                    <li><a href="">Facebook</a></li>
-                    <li><a href="">Twitter</a></li>
-                </ul>
+            <div className="container footer-container">
+                <div className="image-container">
+                    <img className="footerImg" src={logoVertical} alt="Little Lemon logo full size"/>
+                </div>
+                <nav>
+                    <h3>Doormat Navigation</h3>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Reservations</a></li>
+                        <li><a href="#">Order Online</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>Address</li>
+                        <li>Phone number</li>
+                        <li>email</li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h3>Social Media Links</h3>
+                    <ul>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                    </ul>
+                </nav>
             </div>
         </footer>
     )
