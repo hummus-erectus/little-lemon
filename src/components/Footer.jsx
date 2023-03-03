662d3049d50a3c6f91b4d678f141673331ba501b
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-03-03 15:14:41 +0900

    Improved Footer
    Added padding and updated links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,9 @@ function Footer() {
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="">About</Link></li>
                         <li><Link to="">Menu</Link></li>
-                        <li><Link to="">Reservations</Link></li>
-                        <li><Link to="">Order Online</Link></li>
-                        <li><Link to="">Login</Link></li>
+                        <li><Link to="/booking">Reservations</Link></li>
+                        <li><Link to="/order">Order Online</Link></li>
+                        <li><Link to="/login">Login</Link></li>
                     </ul>
                 </nav>
                 <nav>
